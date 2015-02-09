6dbd35dfc78b79d7df3b01622f2d5276c46a0ce8
Daniel Dyba <daniel.dyba@gmail.com>
2015-02-08 19:18:53 -0800

    List items showing class names and memory addresses

--- app/src/main/java/com/danieldyba/criminalintent/fragments/CrimeListFragment.java
+++ app/src/main/java/com/danieldyba/criminalintent/fragments/CrimeListFragment.java
@@ -2,6 +2,7 @@ package com.danieldyba.criminalintent.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
 
 import com.danieldyba.criminalintent.models.Crime;
 import com.danieldyba.criminalintent.managers.CrimeLab;
@@ -21,5 +22,10 @@ public class CrimeListFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),
+                android.R.layout.simple_list_item_1,
+                mCrimes);
+        setListAdapter(adapter);
     }
 }
